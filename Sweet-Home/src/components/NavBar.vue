fa01951e63fa26548348d1be7e3328355ace8d64
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2023-02-17 22:17:04 +0100

    initProject

---
+++ Sweet-Home/src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+
+<template>
+
+<nav>
+      <div>
+
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+            
+      </div>      
+</nav>
+        
+</template>
+
+
+
+<style scoped>
+
+nav{
+  width: 100%;
+  height: 40px;
+  background-color: bisque;
+}
+
+</style>
